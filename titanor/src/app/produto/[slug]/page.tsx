9450a74c645166df21bedb209093daf5ce4aaff7
Guilherme Henrique <97580399+Guilhermehenriquee@users.noreply.github.com>
2026-07-16 22:07:21 -0300

    Delete titanor directory

--- titanor/src/app/produto/[slug]/page.tsx
+++
@@ -1,5 +0,0 @@
-import ProductPage, { generateMetadata, generateStaticParams } from "@/app/produtos/[slug]/page";
-
-export const revalidate = 60;
-export { generateMetadata, generateStaticParams };
-export default ProductPage;
